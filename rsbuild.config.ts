e232e24b9340b44393c3d16c8e4e2a00dab83abc
ndaden <nabil.daden@gmail.com>
2023-12-14 23:20:57 +0100

    fix env things

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,10 +3,25 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  dev: {
+    writeToDisk: false,
+  },
   server: {
     port: 3000,
+    publicDir: {
+      name: "public",
+    },
   },
   html: {
-    template: "./public/index.html",
+    template: "./template/index.html",
+  },
+  output: {
+    distPath: {
+      root: "./build",
+      html: "",
+      css: "",
+      js: "",
+      image: "",
+    },
   },
 });
