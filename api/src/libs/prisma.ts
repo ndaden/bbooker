1894c377d222ba80af5b08c6a44761616e43d51c
Nabil D <nabil.daden@gmail.com>
2026-02-16 23:08:53 +0100

    fix api

--- api/src/libs/prisma.ts
+++ api/src/libs/prisma.ts
@@ -4,6 +4,8 @@ if (!Bun.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
+console.log("Connecting to MongoDB with URL:", Bun.env.DATABASE_URL);
+
 export const prisma = new PrismaClient();
 
 prisma.$connect().catch((error) => {
